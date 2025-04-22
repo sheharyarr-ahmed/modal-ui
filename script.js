2f1ada4fb58c76534e4b2444d43501ec7bd79cec
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-22 15:43:24 +0500

    learned about the classList

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+const btnShowModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const addHidden = function () {
+  overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+};
+
+// console.log(btnShowModal);
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    console.log("modal opened and overlay enabled");
+  });
+
+  btnCloseModal.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+
+  overlay.addEventListener("click", addHidden);
+}
