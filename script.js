d0e908b12a111cd258c3b1dbd1d9b6405aaf92ef
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-23 06:15:05 +0500

    learned aboout keyboard events, manipulation wiht classes by using add, remove and classList and contains methods

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 "use strict";
 const btnShowModal = document.querySelectorAll(".show-modal");
+//in querySelector we use . before specifying any class where as in the manipulation with class using any methods like add, remove, contains we dont use . before specifying any class.
 const modal = document.querySelector(".modal");
 const btnCloseModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
-const addHidden = function () {
+const closeModal = function () {
   overlay.classList.add("hidden");
   modal.classList.add("hidden");
 };
 
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
 // console.log(btnShowModal);
 for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+  btnShowModal[i].addEventListener("click", openModal);
+  {
     console.log("modal opened and overlay enabled");
-  });
+  }
+  // the function inside the event handler was then applied by the fucntion named as addHidden
+  //   btnCloseModal.addEventListener("click", function () {
+  //     modal.classList.add("hidden");
+  //     overlay.classList.add("hidden");
+  //   });
+  btnCloseModal.addEventListener("click", closeModal); //here after the functin name addHidden the parenthesis were not added because otherwise function would be immediately invoked not after happening of the event but immediately
 
-  btnCloseModal.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  });
-
-  overlay.addEventListener("click", addHidden);
+  overlay.addEventListener("click", closeModal);
 }
+//keyboard events look for it in the obsidian
+
+// document.addEventListener("keydown", function () {
+//   console.log("a key was pressed");
+// });
+document.addEventListener("keydown", function (e) {
+  // the parameter e is holding the value of key pressed meaning this parameter is actually the pressed key.
+  console.log(e.key + " was pressed");
+  //   if (e.key === "Escape") {
+  //     if (!modal.classList.contains("hidden")) {
+  //       // condition here: if modal class does not containes hidden
+  //       closeModal();
+  //     }
+  //after refactoring
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
